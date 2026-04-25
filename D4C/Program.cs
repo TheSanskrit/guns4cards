3968823302265917177aaa2bf6ece35d351b6d5f
ruslan <ruslansafican83@gmail.com>
2026-04-25 21:12:52 +0500

    hrth

--- D4C/Program.cs
+++ D4C/Program.cs
@@ -46,6 +46,8 @@ async Task HandleUpdateAsync(ITelegramBotClient bot, Update update, Cancellation
     if (update.Message?.Text == null)
         return;
 
+    Console.WriteLine("Получено сообщение, дебаг");
+
     var text = update.Message.Text;
     var normalized = text.Trim().ToLower();
     var chatId = update.Message.Chat.Id;
