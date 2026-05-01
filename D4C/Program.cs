7a44cd8f3919fb82cd17ffa6e4345db4017ea006
ruslan <ruslansafican83@gmail.com>
2026-05-01 21:48:50 +0500

    returned calcellation token

--- D4C/Program.cs
+++ D4C/Program.cs
@@ -101,7 +101,8 @@ async Task HandleUpdateAsync(ITelegramBotClient bot, Update update, Cancellation
                 EntComs.GiveCard(callback.From.Id, rarity, 100)
             ),
             parseMode: ParseMode.MarkdownV2,
-            replyMarkup: null
+            replyMarkup: null,
+            cancellationToken: token
 
         );
 
